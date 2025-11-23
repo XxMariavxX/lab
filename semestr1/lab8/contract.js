86682396c1256bbacbaf47eac62ff1283dbb0a03
XxMariavxX <synevicmaria13@gmail.com>
2025-11-23 19:21:56 +0200

    endverstion

--- semestr1/lab8/contract.js
+++ semestr1/lab8/contract.js
@@ -2,41 +2,69 @@
 
 console.log("Завдання 3");
 
-// const contract = (fn, ...types) => {
-//     return (...args) => {
-//         args.forEach((elem, index) => {
-//             const expected = types[index];
-//             const actual= Object.prototype.toString.call(elem).slice(8, -1);
-//             if (types[index]) {
-//                 throw new TypeError(`Аргумент ${index + 1} має невірний тип. Очікувався ${type[index].name}`);
-//             }
-//         });
-//         if (types.lenght - 1 != args.length){
-//             throw new Error("Error");
-//         }
-//         console.log(args);
-//     }
-// }
-
 const contract = (fn, ...types) => {
-     return (...args) => {
-        
-     }
+  return (...args) => {
+    if (args.length !== types.length - 1) {
+      throw new TypeError("Incorrect number of symbols");
+    }
+    for (let i = 0; i < args.length; i++) { 
+      const expectedType = types[i].name.toLowerCase();
+      if (typeof args[i] !== expectedType) {
+        throw new TypeError(`Arguments should be ${expectedType}`);
+      }
+    }
+    const result = fn(...args);
 
+    const expectResultType = types[types.length - 1].name.toLowerCase();
+    if (expectResultType !== typeof result) {
+      throw new TypeError(`Result type should be ${expectResultType}`);
+    }
+    return result;
+  };
+};
 
-}
+// const contract = (fn, ...types) => {
+//   return (...args) => {
+//      try{
+//            if (args.length !== types.length - 1) {
+//       throw new TypeError("TypeError");
+//     }
+//     for (let i = 0; i < args.length; i++) { 
+//       const expectedType = types[i].name.toLowerCase();
+//       if (typeof args[i] !== expectedType) {
+//         throw new TypeError("TypeError");
+//       }
+//     }
+//     const result = fn(...args);
+//     const expectResultType = types[types.length - 1].name.toLowerCase();
+//     if (expectResultType !== typeof result) {
+//       throw new TypeError("TypeError");
+//     }
+//     return result;      
+//      }
+//      catch(e) {
+//           console.log("TypeError");
+//           return null;
+//      }
+//   };
+// };
 
+try{
 let add = (a, b) => a + b;
 let addNumbers = contract(add, Number, Number, Number);
 let res = addNumbers(2, 3);
-console.dir(res);// Output: 5
+console.dir(res); // Output: 5
+
+let concat = (s1, s2) => s1 + s2;
+let concatStrings = contract(concat, String, String, String);
+res = concatStrings("Hello ", "world!");
+console.dir(res); // Output: Hello world!
 
 add = (a, b) => a + b;
 addNumbers = contract(add, Number, Number);
 res = addNumbers(2, 3);
-console.dir(res);
-
-concat = (s1, s2) => s1 + s2;
-concatStrings = contract(concat, String, String, String);
-res = concatStrings('Hello ', 'world!');
-console.dir(res); // Output: Hello world!
+console.dir(res); // Output: Incorrect number of symbols
+}
+catch(e){
+     console.error(e);
+}
